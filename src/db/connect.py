8638bb393b8ab71a0edc355c62c0c2ec0fdce61b
jod35 <jodestrevin@gmail.com>
2025-04-18 21:16:34 +0300

    import model

--- src/db/connect.py
+++ src/db/connect.py
@@ -4,4 +4,5 @@ from sqlmodel import SQLModel
 
 async def create_connection() -> None:
     async with engine.begin() as conn:
+        from src.db.models import Book
         await conn.run_sync(SQLModel.metadata.create_all)
